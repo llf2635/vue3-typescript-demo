a7d3f63540253e60956bb4bbc80b0682f842acfa
龙茶清欢 <2320391937@qq.com>
2025-03-03 15:31:08 +0800

    backup

--- demo-vitepress/docs/.vitepress/config.ts
+++ demo-vitepress/docs/.vitepress/config.ts
@@ -6,6 +6,57 @@ export default defineConfig({
   // 配置站点元数据  https://vitepress.dev/zh/reference/site-config#site-metadata
   title: "龙茶清欢",
   description: "vitepress测试",
+  lastUpdated: true,
+
+  // 使用内置的 i18n (国际化) 功能，参考 https://vitepress.dev/zh/guide/i18n
+  locales: {
+    root: {
+      label: '简体中文',
+      lang: 'zh-CN', // 可选，将作为 `lang` 属性添加到 `html` 标签中
+      description: "VitePress Sidebar 是一个 VitePress 插件，只需一次设置即可自动生成侧边栏菜单，没有麻烦。通过轻松为大量文章创建分类来节省时间.",
+      themeConfig: {
+        docFooter: {
+          prev: "上一页",
+          next: "下一页"
+        },
+        outline: {
+          label: "本页内容"
+        },
+        lastUpdated: {
+          text: "最后更新"
+        },
+        langMenuLabel: "更改语言",
+        returnToTopLabel: "返回页首",
+        sidebarMenuLabel: "菜单",
+        darkModeSwitchLabel: "外观",
+        lightModeSwitchTitle: "切换到浅色主题",
+        darkModeSwitchTitle: "切换到深色主题"
+      }
+    },
+    english: {
+      label: "English",
+      lang: "en-US",
+      description: "VitePress Sidebar is a VitePress plugin that automatically generates sidebar menus with one setup and no hassle. Save time by easily creating taxonomies for tons of articles.",
+      themeConfig: {
+        docFooter: {
+          prev: "Previous page",
+          next: "Next page"
+        },
+        outline: {
+          label: "On this page"
+        },
+        lastUpdated: {
+          text: "Last updated"
+        },
+        langMenuLabel: "Change language",
+        returnToTopLabel: "Return to top",
+        sidebarMenuLabel: "Menu",
+        darkModeSwitchLabel: "Appearance",
+        lightModeSwitchTitle: "Switch to light theme",
+        darkModeSwitchTitle: "Switch to dark theme"
+      }
+    },
+  },
 
   // 站点主题
   themeConfig: {
@@ -34,19 +85,32 @@ export default defineConfig({
           {text: 'SpringBoot', link: '/springboot/'}
         ]
       },
-      {text: '指南', link: '/guide/'},
       {
-        text: '组件',
+        text: '数据库',
         items: [
           {
-            text: 'Vue',
+            text: 'MySQL',
             items: [
               {text: 'Markdown Examples', link: '/markdown-examples'},
               {text: 'Runtime API Examples', link: '/api-examples'}
             ]
           },
           {
-            text: 'TypeScript',
+            text: 'PostgreSQL',
+            items: [
+              {text: 'Markdown Examples', link: '/markdown-examples'},
+              {text: 'Runtime API Examples', link: '/api-examples'}
+            ]
+          },
+          {
+            text: 'MongoDB',
+            items: [
+              {text: 'Markdown Examples', link: '/markdown-examples'},
+              {text: 'Runtime API Examples', link: '/api-examples'}
+            ]
+          },
+          {
+            text: 'Redis',
             items: [
               {text: 'Markdown Examples', link: '/markdown-examples'},
               {text: 'Runtime API Examples', link: '/api-examples'}
@@ -136,7 +200,7 @@ export default defineConfig({
         dateStyle: 'full',
         timeStyle: 'medium'
       },
-    }
+    },
 
   }
 })
