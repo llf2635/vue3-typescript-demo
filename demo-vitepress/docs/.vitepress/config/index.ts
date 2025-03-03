fc94aa36e16aac947326ba4b22a9c7e1865124d3
龙茶清欢 <2320391937@qq.com>
2025-03-04 00:10:20 +0800

    backup

--- demo-vitepress/docs/.vitepress/config/index.ts
+++ demo-vitepress/docs/.vitepress/config/index.ts
@@ -7,7 +7,10 @@ import { zhCN } from './zh-CN'
 export default defineConfig({
     ...shared,
     locales: {
-        root: { label: '简体中文', ...zhCN },
-        en: { label: 'English', ...enUS },
+        // VitePress 默认不会将 / 重定向到 /zh/。需要配置服务器来实现这一点。参考  https://vitepress.dev/zh/guide/i18n#separate-directory-for-each-locale
+        root: { label: '简体中文', ...zhCN },   // 默认 / -- 显示在导航栏翻译菜单上，可以是外部的
+        en: { label: 'English', ...enUS },  // 默认 /en/ -- 显示在导航栏翻译菜单上，可以是外部的
+
+        // 其余 locale 特定属性...
     },
 })
