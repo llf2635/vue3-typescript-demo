e7eb9e8018526d7b00d763ee05352ca17e9e6748
龙茶清欢 <2320391937@qq.com>
2025-03-05 01:23:39 +0800

    backup

---
+++ demo-bun/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig(({ command, mode }) => {
+  console.log(command, mode)
+
+  // 加载环境变量：根据 mode 读取 .env.[mode] 文件 使用 process 需要安装 bun add -D @types/bun 或者 @types/node
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log(env)
+
+  return {
+    // vite 配置
+    plugins: [vue()],
+  }
+})
